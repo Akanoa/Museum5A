4ca4358bab9e2d337d49f39c562112bb70e58a15
Yannick Guern <y0guern@enib.fr>
2014-10-11 12:27:42 +0200

    First stable version, adding camera and prims to testing

---
+++ main.py
@@ -0,0 +1,101 @@
+#! /usr/bin/python
+
+import pyglet
+from pyglet.gl import *
+
+from museum import Museum
+
+import math
+
+
+import camera
+
+import xml_tool
+
+myMuseum = None 
+myCamera = None
+window   = None
+
+try:
+	config = Config(sample_buffer=1, samples=4, \
+          depth_size=16, double_buffer=True)
+	window = pyglet.window.Window(resizable=True, config=config)
+	window.set_exclusive_mouse(True)
+except:
+	window = pyglet.window.Window(resizable=True)
+	window.set_exclusive_mouse(True)
+
+def setup():
+	glEnable(GL_DEPTH_TEST)
+	glEnable(GL_TEXTURE_2D)
+	glAlphaFunc(GL_GREATER,0.4)
+	glEnable(GL_ALPHA_TEST)
+
+def init():
+	global myMuseum, myCamera
+	setup()
+	myCamera = camera.FirstPersonCamera(window, mouse_sensitivity=0.02)
+	myMuseum = Museum("museum.xml")
+	myMuseum.init()
+	pass
+
+
+@window.event
+def on_resize(width, height):
+    # Override the default on_resize handler to create a 3D projection
+    glViewport(0, 0, width, height)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    gluPerspective(70.0, width / float(height), .1, 1000.)
+    glMatrixMode(GL_MODELVIEW)
+    return pyglet.event.EVENT_HANDLED
+
+
+@window.event
+def on_draw():
+	global scene
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+	glLoadIdentity()
+	myCamera.draw()
+	#glClearColor(1.0,1.0,1.0,1.0)
+
+	for i in range(3):
+		glTranslatef(i*10, 0, 0)
+		cube()
+
+
+	return pyglet.event.EVENT_HANDLED
+
+def cube():
+	pyglet.gl.glColor4f(1.0,0,0,1.0)
+
+	pyglet.graphics.draw_indexed(8, pyglet.gl.GL_LINES, [0, 1, 1, 2, 2, 3, 3, 0,
+														4, 5, 5, 6, 6, 7, 7, 4,
+														0, 4, 1, 5, 2, 6, 3, 7],
+														('v3f', (-1, -1, 0,
+														1, -1, 0,
+														1, 1, 0,
+														-1, 1, 0,
+														-1, -1, -1,
+														1, -1, -1,
+														1, 1, -1,
+														-1, 1, -1)))
+	
+def update(dt):
+	global horloge
+
+	#horloge = horloge + dt
+	myCamera.update(dt)
+
+
+if __name__ == '__main__' : 
+	print "Hello World"
+	init()
+	# # La fonction update sera appelee toutes les 30eme de seconde
+	pyglet.clock.schedule_interval(update, 1.0/24.0)
+
+	pyglet.app.run()
+
+	#print xml_tool.parse(file("museum.xml"))["xml"]["default"]["dimensions"]["@width"]
+
+
---
+++ museum.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#------------------------------------------------
+#
+#
+#------------------------------------------------
+
+import xml.etree.ElementTree as ET
+import os
+import random
+import struct
+import pprint as p
+
+class Museum:
+
+	'''
+	create a new museum by using config dict
+	'''
+	def __init__(self, config="museum.xml"):
+		self.default_config = ET.fromstring(file(config).read())
+		self.config = {}
+
+	def init(self):
+		self.__generate_room_config()
+
+	'''
+	use default and user specified config to generate correct configs to rooms
+	all parameters can be redefined:
+		- height
+		- length
+		- width
+		- wall_texture
+		- floor_texture
+		- ground_texture
+		- nb_paintings
+	path to parameters in config dict
+	'''
+	def __override_default(self, room_id, args):
+		"""
+		generates given xml tree if it isn't exist 
+		"""
+		saved = None
+		default = self.default_config.findall('./default/'+args)
+		path = "./rooms/room[@id='"+str(room_id)+"']"+args
+		try:
+			custom_root = self.default_config.findall("./rooms/room[@id='"+str(room_id)+"']")[0]
+			custom = self.default_config.findall(path)
+		except:
+			return default[0]
+		if custom != []:
+			saved = custom[0]
+		else:
+			saved = default[0]
+
+		return saved
+
+	def jpeg_res(self, filename):
+		""""This function prints the resolution
+		of the jpeg image file passed into it"""
+
+		# open image for reading in binary mode
+		width = 0
+		height = 0
+		with open(filename,'rb') as jpeg:
+			jpeg.read(2)
+			b = jpeg.read(1)
+			try:
+				while (b and ord(b) != 0xDA):
+					while (ord(b) != 0xFF): b = jpeg.read(1)
+					while (ord(b) == 0xFF): b = jpeg.read(1)
+					if (ord(b) >= 0xC0 and ord(b) <= 0xC3):
+						jpeg.read(3)
+						h, w = struct.unpack(">HH", jpeg.read(4))
+						break
+					else:
+						jpeg.read(int(struct.unpack(">H", jpeg.read(2))[0])-2)
+					b = jpeg.read(1)
+				width = int(w)
+				height = int(h)
+			except struct.error:
+				pass
+			except ValueError:
+				pass
+
+			return (width, height)
+
+
+	def __generate_room_config(self):
+		nb_rooms = int(self.default_config.findall('./default/dimensions')[0].get("nb"))
+		width    = int(self.default_config.findall('./default/dimensions')[0].get("width"))
+		length   = int(self.default_config.findall('./default/dimensions')[0].get("length"))
+		height   = int(self.default_config.findall('./default/dimensions')[0].get("height"))
+
+
+
+
+		gap_association = {
+			"void"   : int(self.default_config.findall("./default/doors_conf/door[@type='void']")[0].get("size")),
+			"normal" : int(self.default_config.findall("./default/doors_conf/door[@type='normal']")[0].get("size")),
+			"big"    : int(self.default_config.findall("./default/doors_conf/door[@type='big']")[0].get("size")),
+			"wall"   : int(self.default_config.findall("./default/doors_conf/door[@type='wall']")[0].get("size"))
+		}
+
+
+		for room_id in range(nb_rooms):
+			try:
+				print "configuring rooms "+str(room_id)
+				#set room
+				self.config[room_id] = {"doors":{}}
+				#set dimensions
+				self.config[room_id]["dimensions"]=(width, length, height)
+				#set north wall
+				self.config[room_id]["doors"]=[]
+				door = self.__override_default(room_id, "/doors/door[@direction='up']")
+				self.config[room_id]["doors"].append({"gap":gap_association[door.get("type")]})
+				#set south wall
+				door = self.__override_default(room_id, "/doors/door[@direction='down']")
+				self.config[room_id]["doors"].append({"gap":gap_association[door.get("type")]})
+				#\tset east wall
+				door = self.__override_default(room_id, "/doors/door[@direction='left']")
+				self.config[room_id]["doors"].append({"gap":gap_association[door.get("type")]})
+				#set east wall
+				door = self.__override_default(room_id, "/doors/door[@direction='right']")
+				self.config[room_id]["doors"].append({"gap":gap_association[door.get("type")]})
+
+				#set numbers of paintings
+				nb = int(self.__override_default(room_id, "/paintings").get("nb"))
+				absolute_path = self.__override_default(room_id, "/paintings").get("path")
+				list_paintings = os.listdir(absolute_path)
+				self.config[room_id]["paintings"]=[]
+				for i in range(nb):
+					path = absolute_path+random.choice(list_paintings)
+
+					self.config[room_id]["paintings"].append([path, self.jpeg_res(path)])
+
+				#set textures
+				self.config[room_id]["textures"]={}
+				#	set wall textures
+				self.config[room_id]["textures"]["wall"] = self.__override_default(room_id, "/textures/texture[@type='walls']").get("path")
+				#	set ground textures
+				self.config[room_id]["textures"]["ground"] = self.__override_default(room_id, "/textures/texture[@type='ground']").get("path")
+				#	set floor textures
+				self.config[room_id]["textures"]["ceiling"] = self.__override_default(room_id, "/textures/texture[@type='ceiling']").get("path")
+
+			except Exception,e:
+				print e
+				pass
+
+
+
+if __name__ == "__main__":
+	m = Museum()
+	m.init()
